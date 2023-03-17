00d9485fb22e3ffc5f250796ec5ae6c80a4fbc74
Aisha Hussein <aishasalaam07@gmail.com>
2023-03-17 04:12:19 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+
+ 
+    const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+  function returnFirstTwoDrivers(people){
+    return people.slice(0,2);
+  }
+// console.log(returnFirstTwoDrivers());
+function returnLastTwoDrivers(kind){
+    return kind.slice(-2,kind.lenght)
+}
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+let fareMultiplier;
+function createFareMultiplier(x){
+return function(){
+    return x*x
+}
+}
+function fareDoubler(pass){
+    return pass * 2
+    
+}
+function fareTripler(milhan){
+    return milhan * 3
+}
+const selectDifferentDrivers = (arrayOfDrivers, gift) => {
+    return  gift(arrayOfDrivers);
+
+    
+}
